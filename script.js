8936cbb33f17f3e7d8d2cd6d7bcfddd6bacc1177
Elton Lac <lacelton@yahoo.com>
2021-01-23 05:12:51 -0800

    Search returns weather info

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
     $("#submitWeather").click(function() {
         var city = $("#city").val();
         if (city !="") {
-
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=0b7c31e0bedb2fd1fc8c4f4b99e83532&units=imperial",
+                type: "GET",
+                dataType:"jsonp",
+                success: function(data){
+                    console.log(data);
+                }
+            });
         } else{
             $("#error").html("Input a city")
         }
