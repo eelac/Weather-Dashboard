3edf2a83430caa59fa7538b3b1181f57d564c4a0
Elton Lac <lacelton@yahoo.com>
2021-01-23 19:45:00 -0800

    Added current weather conditions

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
-$(document).ready(function() {
-    $("#submitWeather").click(function() {
-        var city = $("#city").val();
-        if (city !="") {
-            $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=0b7c31e0bedb2fd1fc8c4f4b99e83532&units=imperial",
-                type: "GET",
-                dataType: "json",
-                success: function(data){
-                    console.log(data);
-                    var widget = show(data);
-                    $("#show").html(widget);
-                    var lat = data.coord.lat;
-                    var lon = data.coord.lon;
-                    getUVIndex(lat, lon);
-                }
-            })
-        }else{
-            $("#error").html("Input a city")
-        }
-    });
+$(document).ready(function () {
+  $("#submitWeather").click(function () {
+    var city = $("#city").val();
+    if (city != "") {
+      $.ajax({
+        url:
+          "http://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&appid=0b7c31e0bedb2fd1fc8c4f4b99e83532&units=imperial",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+          var widget = show(data);
+          $("#show").html(widget);
+          var lat = data.coord.lat;
+          var lon = data.coord.lon;
+          getUVIndex(lat, lon);
+        },
+      });
+    } else {
+      $("#error").html("Input a city");
+    }
+  });
 });
 
 function getUVIndex(lat, lon) {
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=0b7c31e0bedb2fd1fc8c4f4b99e83532",
+  $.ajax({
+    url:
+      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=0b7c31e0bedb2fd1fc8c4f4b99e83532",
     type: "GET",
     dataType: "json",
-    success:function(data){
-        console.log(data.value);
-    }
-})
+    success: function (data) {
+      console.log(data.value);
+    },
+  });
 }
-            
+
 function show(data) {
-    return "<h2 style='font-size:40px; font-weight: bold;'>Current weather for " + data.name +
-        "<h3>Temperature: " + data.main.temp + " &deg;F" +
-        "<h3>Humidity: " + data.main.humidity + "%" +
-        "<h3>Wind Speed: " + data.wind.speed + " MPH" +
-        "<h3>UV Index: " + data.value;
+  return (
+    "<h2 style='font-size:40px; font-weight: bold;'>Current weather for " +
+    data.name +
+    "<h3>Description: <img src='http://openweathermap.org/img/wn/" +
+    data.weather[0].icon +
+    "@2x.png'>" +
+    data.weather[0].main +
+    "<h3>Temperature: " +
+    data.main.temp +
+    " &deg;F" +
+    "<h3>Humidity: " +
+    data.main.humidity +
+    "%" +
+    "<h3>Wind Speed: " +
+    data.wind.speed +
+    " MPH" +
+    "<h3>UV Index: " +
+    data.value
+  );
 }
